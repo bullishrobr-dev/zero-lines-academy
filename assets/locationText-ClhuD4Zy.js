b15d95b056793a5485c331841ecc6e8e3c61bc91
Zero Lines Deploy <bullishrobr@gmail.com>
2026-08-06 16:30:19 +0000

    Deploy build 20: docked footer, onboarding facelift

--- assets/locationText-ClhuD4Zy.js
+++
@@ -1,1 +0,0 @@
-import{q as o}from"./index-Dl0yndLi.js";function l(){const{currency:r,locationName:a,taxHavenText:c}=o();return{replacePlaceholders:e=>e&&e.replace(/\{\{currency\}\}/g,r).replace(/\{\{location\}\}/g,a).replace(/\{\{taxHavenText\}\}/g,c),currency:r,location:a,taxHavenText:c}}export{l as u};
